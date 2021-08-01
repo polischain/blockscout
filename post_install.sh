039f11b2c492545344f8a5d3e54829c0dcfe56e8
eabz <eabz@polispay.org>
2021-08-01 18:05:21 -0500

    Just migrate

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Migrate
-mix do ecto.create, ecto.migrate
+mix do ecto.migrate
 
 # Build static assets
 cd apps/block_scout_web/assets && npm install && node_modules/webpack/bin/webpack.js --mode production
