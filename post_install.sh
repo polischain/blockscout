dcc546be0a3cc0568120b9aad78fc6244898310a
eabz <eabz@polispay.org>
2021-08-01 18:08:34 -0500

    Fixes

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,7 @@
 mix do ecto.migrate
 
 # Build static assets
-cd apps/block_scout_web/assets && npm install && node_modules/webpack/bin/webpack.js --mode production
-
+cd apps/block_scout_web/assets; npm install && node_modules/webpack/bin/webpack.js --mode production; cd -
+cd apps/explorer && npm install; cd -
+mix phx.digest
 
