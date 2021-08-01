ac6d163b7037ea2a7c959305067add8c356f74ff
eabz <eabz@polispay.org>
2021-08-01 16:34:12 -0500

    Update dep

--- apps/ethereum_jsonrpc/mix.exs
+++ apps/ethereum_jsonrpc/mix.exs
@@ -77,7 +77,7 @@ defmodule EthereumJsonrpc.MixProject do
       # Convert unix timestamps in JSONRPC to DateTimes
       {:timex, "~> 3.6"},
       # Encode/decode function names and arguments
-      {:ex_abi, "~> 0.4"},
+      {:ex_abi, "~> 0.5.5"},
       # `:verify_fun` for `Socket.Web.connect`
       {:ssl_verify_fun, "~> 1.1"},
       # `EthereumJSONRPC.WebSocket`
